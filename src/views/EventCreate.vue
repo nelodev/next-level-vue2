7e6e495519dbb2ea40cbd2e590701a529b3ce020
Nelo Puchades <correo@nelo.dev>
2021-10-25 00:18:20 +0200

    Created BaseInput component

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -8,21 +8,12 @@
       </select>
 
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
+      <base-input label="Title" v-model="event.title" type="text" placeholder="Title" class="field" />
 
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
+      <base-input label="Description" v-model="event.description" type="text" placeholder="Description" class="field" />
 
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
+      <base-input label="Location" v-model="event.location" type="text" placeholder="Location" class="field" />
 
       <h3>When is your event?</h3>
 
