5e1953d64e7f3fa6d5ae028e72e916fa549b16e7
Nelo Puchades <correo@nelo.dev>
2021-10-27 00:15:46 +0200

    Used vuelidate to validate form

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -6,7 +6,15 @@
         label="Select a category"
         :options="categories"
         v-model="event.category"
+        :class="{ error: $v.event.category.$error }"
+        @blur="$v.event.category.$touch()"
       />
+
+      <template v-if="$v.event.category.$error">
+        <p v-if="!$v.event.category.required" class="errorMessage">
+          Category is required
+        </p>
+      </template>
 
       <h3>Name & describe your event</h3>
       <base-input
@@ -15,7 +23,15 @@
         type="text"
         placeholder="Title"
         class="field"
+        :class="{ error: $v.event.title.$error }"
+        @blur="$v.event.title.$touch()"
       />
+
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">
+          Title is required
+        </p>
+      </template>
 
       <base-input
         label="Description"
@@ -23,7 +39,15 @@
         type="text"
         placeholder="Description"
         class="field"
+        :class="{ error: $v.event.description.$error }"
+        @blur="$v.event.description.$touch()"
       />
+
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">
+          Description is required
+        </p>
+      </template>
 
       <h3>Where is your event?</h3>
       <base-input
@@ -32,23 +56,58 @@
         type="text"
         placeholder="Location"
         class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.$touch()"
       />
+
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">
+          Location is required
+        </p>
+      </template>
 
       <h3>When is your event?</h3>
 
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
+        <datepicker
+          v-model="event.date"
+          placeholder="Select a date"
+          :input-class="{ error: $v.event.date.$error }"
+          @opened="$v.event.date.$touch()"
+        />
       </div>
+
+      <template v-if="$v.event.date.$error">
+        <p v-if="!$v.event.date.required" class="errorMessage">
+          Date is required
+        </p>
+      </template>
 
       <base-select
         label="Select a time"
         :options="times"
         v-model="event.time"
         class="field"
+        :class="{ error: $v.event.time.$error }"
+        @blur="$v.event.time.$touch()"
       />
 
-      <base-button type="submit" buttonClass="-fill-gradient">Submit</base-button>
+      <template v-if="$v.event.time.$error">
+        <p v-if="!$v.event.time.required" class="errorMessage">
+          Time is required
+        </p>
+      </template>
+
+      <base-button
+        type="submit"
+        buttonClass="-fill-gradient"
+        :disabled="$v.$anyError"
+        >Submit</base-button
+      >
+      <p v-if="$v.$anyError" class="errorMessage">
+        Please fill out the required field(s).
+      </p>
     </form>
   </div>
 </template>
@@ -57,6 +116,7 @@
 <script>
 import Datepicker from 'vuejs-datepicker'
 import nProgress from 'nprogress'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   components: {
@@ -73,8 +133,21 @@ export default {
       event: this.createFreshEventObject(),
     }
   },
+  validations: {
+    event: {
+      category: { required },
+      title: { required },
+      description: { required },
+      location: { required },
+      date: { required },
+      time: { required },
+    },
+  },
   methods: {
     createEvent() {
+      this.$v.$touch()
+      if (this.$v.$invalid) return
+
       nProgress.start()
       this.$store
         .dispatch('event/createEvent', this.event)
@@ -111,6 +184,12 @@ export default {
 </script>
 
 <style scoped>
+.error {
+  border: 1px solid red;
+}
+.errorMessage {
+  color: red;
+}
 .field {
   margin-bottom: 24px;
 }
